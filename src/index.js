c22bd5c773fd63a46293b62e4cde619a2a9a3ff7
Amin Karaji <amin.krj@gmail.com>
2017-10-25 08:46:13 +1100

    Adding font loader to load fonts from Google fonts

--- src/index.js
+++ src/index.js
@@ -4,5 +4,13 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Open+Sans:400,700,300', 'Rambla:400,700', 'Calligraffitti', 'Roboto+Slab:400,700']
+  }
+});
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
