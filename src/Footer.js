a83ff5c144e3b73c3371af43c66a2ef09edc51f5
Amin Karaji <amin.krj@gmail.com>
2017-11-28 16:08:34 +1100

    UI changes

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import cachios from 'cachios';
 import axios from 'axios';
 import NProgress from 'nprogress';
 import iziToast from 'izitoast';
@@ -19,7 +20,7 @@ class Footer extends Component {
   componentWillMount() {
     NProgress.start()
 
-    axios.get("/campaigns/")
+    cachios.get("/campaigns/")
     .then((response) => {
       NProgress.done()
 
