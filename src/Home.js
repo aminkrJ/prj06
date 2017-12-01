5a53b3e8db9eec9f8daee4dbc2296b0ba9e0c193
Amin Karaji <amin.krj@gmail.com>
2017-12-01 13:08:10 +1100

    fix a mistake

--- src/Home.js
+++ src/Home.js
@@ -53,7 +53,7 @@ class Home extends Component {
         })
 
       return (
-        <Carousel id="featured-carousel" margin={2} responsive={ {"0": {"items": 1}, "576": {"items": 2}, "768": {"items": 3}, "991": {"items": 5}, "1200": {"items": 5}} }>
+        <Carousel id="featured-carousel" margin={2} responsive={ {"0": {"items": 1}, "576": {"items": 2}, "768": {"items": 3}, "991": {"items": 4}, "1200": {"items": 4}} }>
         {menu}
         </Carousel>
       )
