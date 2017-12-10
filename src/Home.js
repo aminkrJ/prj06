6bbd36eb0f1d43546a5e45203825e5112ab823f7
Amin Karaji <amin.krj@gmail.com>
2017-12-11 09:57:57 +1100

    Tagline change

--- src/Home.js
+++ src/Home.js
@@ -54,9 +54,9 @@ class Home extends Component {
                 <div class="row justify-content-center align-items-center">
                   <div class="col-lg-6 col-md-6 padding-bottom-1x text-md-left text-center">
                     <div class="from-bottom">
-                      <h1 className="font-weight-bold text-uppercase text-black mb-2 pt-1">Homemade Smoothies</h1>
+                      <h1 className="font-weight-bold text-uppercase text-black mb-2 pt-1">Elevated Smoothies</h1>
                       <hr className="hr-inverse hr-lg w-100 mx-auto my-4" />
-                      <h3 class="tp-caption mb-4 pb-1 font-weight-light text-black tagline">Healthy smoothies packed with superfoods, functional herbs, and medicinal mushrooms</h3>
+                      <h3 class="tp-caption mb-4 pb-1 font-weight-light text-black tagline">Healthy homemade smoothies packed with superfoods, functional herbs, probiotics, and medicinal mushrooms.</h3>
                     </div>
                     <Link class="btn btn-primary btn-rounded py-lg-3 px-lg-5" to="/plans">Order Now</Link>
                     <Link class="btn btn-link btn-rounded py-lg-3 px-lg-3" to="/about">Learn More</Link>
