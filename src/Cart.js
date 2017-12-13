9aecd01fb78824f2118c813da491a865b14e1217
Amin Karaji <amin.krj@gmail.com>
2017-12-13 11:22:25 +1100

    increase delivery fee

--- src/Cart.js
+++ src/Cart.js
@@ -10,7 +10,7 @@ class Cart extends Component {
       product: {photo: {}},
       unitPrice: 0,
       quantity: 1,
-      shippingFee: 5,
+      shippingFee: 8,
       total: 0,
       isSending: false,
       errors: {}
