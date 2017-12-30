dc9ffb37716f1e7a21e42f85fd907722a58f54ce
Amin Karaji <amin.krj@gmail.com>
2017-12-30 16:58:01 +1100

    Delivery date text update

--- src/Delivery.js
+++ src/Delivery.js
@@ -19,11 +19,12 @@ class Delivery extends Component {
                 <AboutSideMenu active={2} />
               </div>
               <div class="col-md-9">
-                <p class="lead">We deliver raw and fresh smoothies to your doorstep.</p>
+                <p class="lead">We deliver fresh food to your doorstep.</p>
                 <p>We are committed to freshness. We managed to keep the price of our delivery $8 in selected suburbs in Sydney. We are aiming to expand our network across all suburbs in Sydney. Sign up to our newsletter to get the latest news about our mobile places and delivery suburbs in your inbox.</p>
-                <h4>Customised delivery date and time</h4>
-                <p>Simply, <Link to="/bundles">pick your bundle</Link> and check for availability of your delivery time.</p>
-                <p>If you do not open the door or respond to telephonic correspondence within 10 minutes of our delivery drivers physically reaching your address, we reserve the right to leave the premises, and you will be charged for the order.</p>
+
+                <h4 class="text-slab">Customised delivery date</h4>
+                <p>Simply, <Link to="/menu">pick your favorites</Link> and check for availability of your delivery date.</p>
+                <p class="text-sm text-muted">Note: If you do not open the door or respond to telephonic correspondence within 10 minutes of our delivery drivers physically reaching your address, we reserve the right to leave the premises, and you will be charged for the order.</p>
               </div>
             </div>
           </div>
