531e8f4cbcdebfd336875b312e51519423560ded
Amin Karaji <amin.krj@gmail.com>
2018-01-24 13:25:44 +1100

    txt update

--- src/About.js
+++ src/About.js
@@ -34,10 +34,10 @@ class About extends Component {
               </p>
               <p>
 
-Let me tell you a secret. Food and diet plans forge our reality. Yes, it does. That being said, it is not a quick process. it demands time and patience. However, in our busy lifestyle, it is difficult to stick to a dietary plan. Why? because it is nearly impossible to make all the meals at home. Besides, eating out is not an option since today restaurants menu are designed to be affordable and liked by the majority not to fulfil a personalised dietary plan. As a result, we repeatedly go on a diet and give up. 
+We believe food is the secret to shape our reality. However, in our busy lifestyle, it is difficult to have a meal program let alone sticking to it. Moreover, restaurants cannot be designed to fulfill various dietary requirments.
               </p>
       <p>
-In lifelixir, we found this growing need and we built an integrated menu for various dietary requirements like enhancing memory and focus, boosting endurance, expanding consciousness, losing weight, detoxing body, having a healthy pregnancy, having healthy skin, and having a healthy gut and make it available in selected restaurants.
+In lifelixir, we found this growing need and we built an integrated menu for various dietary requirements like enhancing memory and focus, boosting endurance, expanding consciousness, losing weight, detoxing body, having a healthy pregnancy, having healthy skin, and having a healthy gut and we made it available in your favorite restaurants in your vicinity.
       </p>
               <p>
                We make the personalised meals readily available everywhere for all dietary requirments.
