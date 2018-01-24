3937e017fbe1c416dd8f38477d5383b4cc09f1b9
Amin Karaji <amin.krj@gmail.com>
2018-01-24 17:10:21 +1100

    removing about us link from footer

--- src/Footer.js
+++ src/Footer.js
@@ -144,7 +144,6 @@ Nutritions your competitors consume to be one step ahead in the game.
                   Company
                 </h4>
                 <ul class="list-unstyled footer-links">
-                  <li><Link to="/about">About us</Link></li>
                   <li><Link to="/contact">Contact us</Link></li>
                 </ul>
               </div>
