ee8e10bbec4b3bd1c2b673db2dc363582ea180a1
Amin Karaji <amin.krj@gmail.com>
2018-01-24 13:03:11 +1100

    update about page

--- src/About.js
+++ src/About.js
@@ -33,7 +33,8 @@ class About extends Component {
               Restaurants with integrated menu specialised in various dietary requirements, everywhere for everyone!
               </p>
               <p>
-Let me tell you a secret. Food and diet plans forge our reality. Yes, it does. That being said, it is not a quick process. it demands time and patience. However, in our busy lifestyle, food is not our first priority or we cannot stick to a dietary plan since it is nearly impossible to make all the meals at home. Moreover, dining out is not an option since today restaurants are designed to serve the public with affordable places. Not you with your personalised dietary plans. As a result, we do not get the result and give up.
+
+Let me tell you a secret. Food and diet plans forge our reality. Yes, it does. That being said, it is not a quick process. it demands time and patience. However, in our busy lifestyle, it is difficult to stick to a dietary plan. Why? because it is nearly impossible to make all the meals at home. Besides, eating out is not an option since today restaurants menu are designed to be affordable and liked by the majority not to fulfil a personalised dietary plan. As a result, we repeatedly go on a diet and give up. 
               </p>
       <p>
 In lifelixir, we found this growing need and we built an integrated menu for various dietary requirements like enhancing memory and focus, boosting endurance, expanding consciousness, losing weight, detoxing body, having a healthy pregnancy, having healthy skin, and having a healthy gut and make it available in selected restaurants.
