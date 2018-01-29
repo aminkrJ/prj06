54d7753b8accbac43235a104f08db1c009a575f0
Amin Karaji <amin.krj@gmail.com>
2018-01-29 18:15:39 +1100

    SEO page update

--- src/globals.js
+++ src/globals.js
@@ -1,10 +1,10 @@
 const globals = {
-  brand: "NutriCombo",
-  tagline: "Smoothies for fitness and wellbeing",
-  title: "NutriCombo | Smoothies for fitness and wellbeing",
+  brand: "Lifelixir",
+  tagline: "Elixirs for beauty, brain, body and spirit",
+  title: "Juice and smoothie bar | Elixirs for beauty, brain, body and spirit",
   description: "Elixirs to not only taste extraordinary but also to work synergistically to heal and enhance your beauty, brain, body and spirit at the deepest level packed with superfoods, functional herbs, medicinal mushrooms.",
   mission: "",
-  keywords: "juice, smoothie bar, healthy smoothies"
+  keywords: "juice bar, smoothie bar, healthy smoothies"
 }
 export default globals
 
