3ec2b132021c0874a32c1c45d226c31d91a396b4
Amin Karaji <amin.krj@gmail.com>
2018-02-06 20:39:54 +1100

    more on Life Elixir

--- src/About.js
+++ src/About.js
@@ -16,16 +16,17 @@ class About extends Component {
           <div class="container">
             <hr class="hr-lg mt-0 mb-3 w-10 mx-auto hr-primary" />
             <h3 class="text-center font-weight-bold my-0 text-slab">
-      {globals.title}
+      {globals.about.title}
             </h3>
             <hr class="mb-5 w-50 mx-auto" />
             <div class="row">
               <div class="col-md-6">
                 <p class="lead">
-      We design tonics and elixirs to heal and improve your body which eventually leads to higher cognitive function.
+      We design tonics and elixirs to heal and improve your body and your brain.
                 </p>
+                <p></p>
                 <p class="">
-      There is a direct connection between what we eat and how we feel. We may feel having occasional headaches, joint pains, bloating, brain fogs, chronic fatigues, leaky guts, depression, insomnia, being moody are normal but it is not. In fact it is our brain defence system for creating a new normal to burn less energy. When you put a frog into cold water and turn the heat up, they boil to death. Whereas, when you throw a frog in boiling water, they jump right out.
+      There is a direct connection between what we eat and how we feel. We may feel having occasional headaches, joint pains, bloating, brain fogs, chronic fatigues, leaky guts, depression, insomnia, being moody are normal but it is not. In fact it is our brain defence system for creating a new normal to burn less energy.
 </p>
 <p>
 We all know about our mind-body connection and how our thoughts, beliefs can impact our biology. Body-mind connection  is another side of the coin. What we eat, our sleep, our nutrient level, our gut flora, infections to name a few will change the way our brain function.
@@ -39,30 +40,11 @@ Life Elixir was created by Amin Karaji: a software engineer and an entrepreneur.
   <p>
   Soon enough he realised that this is not only a one-way road. In fact, our body can massively impact our mind, conciseness, thoughts and even our values. Lifelixir is a journey of discovering nutritions to hack, heal, and boost your biology to get the peak performance of your body and brain.
                     </p>
- 
-
               </div>
               <div class="col-md-6">
-                <ul class="fa-ul list-unstyled mt-4 mt-md-0">
-                  <li class="mb-4">
-                    <h4 class="mb-1 text-slab">
-      How to get our products
-                    </h4>
-      <p>
-        <ul>
-          <li>
-        Order it in your favorite cafe and restaurants in your area. <Link to='/find'>Find us in your area.</Link>
-          </li>
-          <li>
-       Pick up our <Link to='/nutricombo'>NutriCombo</Link> in health food stores in your area. <Link to='/find'>Find us in your area.</Link>
-          </li>
-      <li>
-      Order our concoctions online. <Link to='/shop'>Shop Now!</Link>
-      </li>
-      </ul>
+                <h4 class="mb-1 text-slab">What is Elixir of Life?</h4>
+                <p>Some sources believed the <a href="https://en.wikipedia.org/wiki/Elixir_of_life" target="_blank">Elixir of Life</a> was a magic potion that was drunk from the Holy Grail to provide immortality. In the scriptures of the Hindus, the Elixir of Life is called Amrita. This was a drink for the gods made from the ocean of milk that provided immortality. In Greek texts, the gods drank Nectar or Ambrosia, which provided them with eternal life. Alchemists also present many theories, or symbolic representations and ciphers, on the Elixir of Life.
       </p>
-                 </li>
-                </ul>
               </div>
             </div>
           </div>
--- src/globals.js
+++ src/globals.js
@@ -1,13 +1,16 @@
 const globals = {
   brand: "Lifelixir",
-  tagline: "",
-  title: "Life Elixir | Effective formulas for health, fitness and beauty",
+  tagline: "Elixirs for health, fitness and beauty",
+  title: "Life Elixir | Elixirs for health, fitness and beauty",
   description: "Elixirs to not only taste extraordinary but also to work synergistically to heal and enhance your beauty, brain, body and spirit at the deepest level to improve your performance packed with superfoods, functional herbs, medicinal mushrooms.",
   mission: "",
-  keywords: "nutricombo, juice bar, smoothie bar, healthy smoothies, elixir of life",
+  keywords: "nutricombo, juice bar, smoothie bar, healthy smoothies, elixir of life, life elixir",
   nutricombo: {
     title: "Life Elixir | NutriCombo superfood healthy breakfast",
     description: "Leaner, stronger and more focused and productive with our plant-based, healthy, and raw breakfast collection packed with superfoods, functional herbs, probiotics and medicinal mushrooms."
+  },
+  about: {
+    title: "10X more done!"
   },
   blog: {
     title: "Life Elixir | Blog",
