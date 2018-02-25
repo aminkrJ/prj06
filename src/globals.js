39e44c174acee0d384cb21e47fd0da04e4eb4ca6
Amin Karaji <amin.krj@gmail.com>
2018-02-26 08:54:46 +1100

    txt update

--- src/globals.js
+++ src/globals.js
@@ -8,7 +8,7 @@ const globals = {
   nutricombo: {
     tagline: "Leaner, stronger and more focused with NutriCombo",
     title: "NutriCombo | Leaner, stronger and more focused",
-    description: "Do not have time to make it at home? NutriCombo menu packed with superfoods, functional herbs, probiotics, and medicinal mushrooms can be ordered in selected cafes in NSW."
+    description: "Looking for healthy options but do not have time to make it at home? NutriCombo menu is available in selected cafes in NSW."
   },
   about: {
     title: "10X more done!"
