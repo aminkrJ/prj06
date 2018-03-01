b2bb6cfbf9efd16d73c2b2d6f48da02aa4c97764
Amin Karaji <amin.krj@gmail.com>
2018-03-01 18:24:53 +1100

    txt changes

--- src/globals.js
+++ src/globals.js
@@ -1,14 +1,14 @@
 const globals = {
   brand: "Lifelixir",
   tagline: "Feel the result",
-  title: "Life Elixir | Nutritions to improve your mental and emotional well-being",
-  description: "Nutritions to help individuals rise above stress, overwhelm, and challenges, promote mental acuteness and overall performance, and replace stressful thoughts and emotional patterns with more positive perceptions and emotions.",
+  title: "Life Elixir | Nutritions to improve your mental and physical well-being",
+  description: "Nutritions to help individuals rise above stress, overwhelm, and life challenges and promote mental acuteness and overall well being and performance.",
   mission: "",
   keywords: "nutricombo, life elixir, food for brain, fitness, health",
   nutricombo: {
     tagline: "Leaner, stronger, motivated and more focused",
     title: "NutriCombo | Leaner, stronger, motivated and more focused",
-    description: "NutriCombo is quick, all-in-one and effective recipes to fuel your body and mind. We harness the power of superfoods, functional herbs and extracts, medicinal mushrooms, probiotics and healthy fats to deliver fuel for the peak performance."
+    description: "NutriCombo is quick, all-in-one and effective recipes to fuel your body and mind packed with superfoods, functional herbs and extracts, medicinal mushrooms, probiotics and healthy fats."
   },
   about: {
     title: "10X more done!"
