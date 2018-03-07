3be01702e285616bb90482f1bf6a04f63105ae49
Amin Karaji <amin.krj@gmail.com>
2018-03-07 21:10:18 +1100

    normal font for footer

--- src/Footer.js
+++ src/Footer.js
@@ -92,7 +92,7 @@ Nutritions your competitors consume to be one step ahead in the game.
                 <h4 class="text-uppercase text-white">
                   Newsletter
                 </h4>
-                <p class='text-sm'><span class='text-sm font-weight-bold text-uppercase'>Get 10% off from your first order.</span> Plus be the first to know about our exclusive offers and deals, special events, and product releases!</p>
+                <p class='text-sm'><span class=''>Get 10% off from your first order.</span> Plus be the first to know about our exclusive offers and deals, special events, and product releases!</p>
 <Subscribe />
               </div>
             </div>
