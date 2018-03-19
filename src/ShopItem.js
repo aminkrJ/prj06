98a6557e86b062b44e60803c999c718510f8f217
Amin Karaji <amin.krj@gmail.com>
2018-03-20 09:22:44 +1100

    react-loader removed was not compatible with Google SEO

--- src/ShopItem.js
+++ src/ShopItem.js
@@ -4,7 +4,6 @@ import api from './Api.js'
 import classnames from 'classnames'
 import NProgress from 'nprogress'
 import AddToCartButton from './AddToCartButton.js';
-import Loader from 'react-loader';
 import {Helmet} from "react-helmet"
 import {render} from 'react-dom';
 import _ from 'underscore'
@@ -18,8 +17,7 @@ class ShopItem extends Component {
   constructor(props){
     super(props)
     this.state = {
-      product: {photo: {original: ""}, recipes: [], tags: [], ingredients: [], category: {}},
-      loaded: false
+      product: {photo: {original: ""}, recipes: [], tags: [], ingredients: [], category: {}}
     }
   }
   
@@ -32,8 +30,7 @@ class ShopItem extends Component {
       NProgress.done()
 
       this.setState({
-        product: response.data,
-        loaded: true
+        product: response.data
       })
     })
     .catch((error) => {
@@ -135,86 +132,84 @@ class ShopItem extends Component {
           <meta name="keywords" content={tags.join(', ')}/>
         </Helmet>
         <div class="container">
-          <Loader loaded={this.state.loaded}>
-            <div class="row">
-              <div class="col-lg-5">
-                <div class="product-gallery pos-relative">
-        <img src={this.state.product.photo.original} class='img-fluid'/>
-                </div>
-                <div class='mt-3'>
-                </div>
+          <div class="row">
+            <div class="col-lg-5">
+              <div class="product-gallery pos-relative">
+      <img src={this.state.product.photo.original} class='img-fluid'/>
               </div>
-              <div class="col-lg-7">
-                <div class="product-card mb-4">
-                  <div class="card-body p-0 px-4 pos-relative">
-                    <div class="pos-md-absolute pos-t pos-r mr-4 mt-3 text-md-right">
-                    </div>
-                    <h2 class="text-capitalize text-uppercase card-title mb-2">
-                      {this.state.product.name}
-                    </h2>
-                    <p dangerouslySetInnerHTML={{__html: this.state.product.short_description}} class="lead" />
-                    <hr class="my-3" />
-                    <div class='row'>
-                      <div class='col-auto mr-auto'>
-                        <h4 class='font-weight-light my-2 text-primary'>
-                          {this.state.product.before_discount_price !== 0 ? <span><del class='text-danger'>${this.state.product.before_discount_price}</del> / </span> : null}
-                          ${this.state.product.price}
-      {this.state.product.category.name === 'Bundles' ? null : <span class='text-black text-xs'>~ {Math.round(this.state.product.weight / this.state.product.serving_size)} servings</span>}
-                        </h4>
-                      </div>
-                      <div class='col-auto'>
-                        <h4 class='font-weight-light my-2'>{this.state.product.category.name === 'Bundles' ? null : <span>{this.state.product.weight}{this.state.product.unit}</span>}</h4>
-                      </div>
-                      <div class='col-auto'>
-                        <AddToCartButton class='btn btn-primary m-0' product={this.state.product} dropFromCart={this.props.dropFromCart} cart={this.props.cart} addToCart={this.props.addToCart} removeFromCart={this.props.removeFromCart} />
-                      </div>
-                    </div>
-                    <hr class="my-3" />
-                    <p dangerouslySetInnerHTML={{__html: this.state.product.description}} class="" />
-                    <p class='mb-1'>
-                    {this.renderTags()}
-                    </p>
-                    <div class="card-accordion card-accordion-list-style card-accordion-icons-left" role="tablist" aria-multiselectable="true">
-                      {this.renderIngredients()}
-                      <div class="card">
-                        <div class='text-sm text-uppercase font-weight-bold my-2 card-header py-0 px-0'>
-                        Serving ideas
-                        </div>
-                        <div class='collapse show'>
-                    <p dangerouslySetInnerHTML={{__html: this.state.product.serving_idea}} class="text-sm" />
-                        </div>
-                      </div>
-                      <div class="card">
-                        <div class='text-sm text-uppercase font-weight-bold my-2 card-header py-0 px-0'>
-                        NutriCombo
-                        </div>
-                        <div class='collapse show'>
-                          <div class=''>
-                          <div class='row'>
-                          {this.renderRecipes()}
-                          </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-
-                <div class='my-2 text-xs font-weight-bold text-uppercase'>{this.renderProps()}</div>
-<div class='text-xs my-2 text-muted'>Note: These statements have not been evaluated by food and drug administration. This product is not intended to diagnose, treat, cure or prevent any disease.</div>
+              <div class='mt-3'>
+              </div>
+            </div>
+            <div class="col-lg-7">
+              <div class="product-card mb-4">
+                <div class="card-body p-0 px-4 pos-relative">
+                  <div class="pos-md-absolute pos-t pos-r mr-4 mt-3 text-md-right">
                   </div>
+                  <h2 class="text-capitalize text-uppercase card-title mb-2">
+                    {this.state.product.name}
+                  </h2>
+                  <p dangerouslySetInnerHTML={{__html: this.state.product.short_description}} class="lead" />
+                  <hr class="my-3" />
+                  <div class='row'>
+                    <div class='col-auto mr-auto'>
+                      <h4 class='font-weight-light my-2 text-primary'>
+                        {this.state.product.before_discount_price !== 0 ? <span><del class='text-danger'>${this.state.product.before_discount_price}</del> / </span> : null}
+                        ${this.state.product.price}
+    {this.state.product.category.name === 'Bundles' ? null : <span class='text-black text-xs'>~ {Math.round(this.state.product.weight / this.state.product.serving_size)} servings</span>}
+                      </h4>
+                    </div>
+                    <div class='col-auto'>
+                      <h4 class='font-weight-light my-2'>{this.state.product.category.name === 'Bundles' ? null : <span>{this.state.product.weight}{this.state.product.unit}</span>}</h4>
+                    </div>
+                    <div class='col-auto'>
+                      <AddToCartButton class='btn btn-primary m-0' product={this.state.product} dropFromCart={this.props.dropFromCart} cart={this.props.cart} addToCart={this.props.addToCart} removeFromCart={this.props.removeFromCart} />
+                    </div>
+                  </div>
+                  <hr class="my-3" />
+                  <p dangerouslySetInnerHTML={{__html: this.state.product.description}} class="" />
+                  <p class='mb-1'>
+                  {this.renderTags()}
+                  </p>
+                  <div class="card-accordion card-accordion-list-style card-accordion-icons-left" role="tablist" aria-multiselectable="true">
+                    {this.renderIngredients()}
+                    <div class="card">
+                      <div class='text-sm text-uppercase font-weight-bold my-2 card-header py-0 px-0'>
+                      Serving ideas
+                      </div>
+                      <div class='collapse show'>
+                  <p dangerouslySetInnerHTML={{__html: this.state.product.serving_idea}} class="text-sm" />
+                      </div>
+                    </div>
+                    <div class="card">
+                      <div class='text-sm text-uppercase font-weight-bold my-2 card-header py-0 px-0'>
+                      NutriCombo
+                      </div>
+                      <div class='collapse show'>
+                        <div class=''>
+                        <div class='row'>
+                        {this.renderRecipes()}
+                        </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+              <div class='my-2 text-xs font-weight-bold text-uppercase'>{this.renderProps()}</div>
+<div class='text-xs my-2 text-muted'>Note: These statements have not been evaluated by food and drug administration. This product is not intended to diagnose, treat, cure or prevent any disease.</div>
                 </div>
               </div>
             </div>
-            <div class="col-lg-12">
-              <hr className="hr-lg mt-0 mb-3 w-10 mx-auto hr-primary" />
-              <h3 className="text-center text-uppercase font-weight-bold my-0">
-      You might also like
-              </h3>
-              <hr className="mb-2 w-50 mx-auto" />
-              <ProductsCarousel shop={_.filter(this.props.featured, (i) => {
-                return i.id !== this.state.product.id
-              })} withAddBtn={false} />
-            </div>
-          </Loader>
+          </div>
+          <div class="col-lg-12">
+            <hr className="hr-lg mt-0 mb-3 w-10 mx-auto hr-primary" />
+            <h3 className="text-center text-uppercase font-weight-bold my-0">
+    You might also like
+            </h3>
+            <hr className="mb-2 w-50 mx-auto" />
+                        {this.state.product.id ? <ProductsCarousel shop={_.filter(this.props.featured, (i) => {
+              return i.id !== this.state.product.id
+            })} withAddBtn={false} /> : null }
+          </div>
         </div>
       </div>
     )
