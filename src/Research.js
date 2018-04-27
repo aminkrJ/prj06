9ced464995d0caa114eef47f6bf011578f7ff18f
Amin Karaji <amin.krj@gmail.com>
2018-04-27 10:24:34 +1000

    update researches

--- src/Research.js
+++ src/Research.js
@@ -31,99 +31,9 @@ class Research extends Component {
               <p>The body breaks down proteins into building blocks called amino acids. Amino acids are a major component in the hundreds of brain chemicals known as neurotransmitters. Neurotransmitters enable brain cells to communicate with each other. Examples of common neurotransmitters include dopamine, serotonin, GABA, norepinephrine, and endorphins.</p>
               <a class='' name='detox'><h5 class='text-uppercase'>Detox body & brain</h5></a>
               <p>Detoxification helps our body to get nutrition from the food we eat and get rid of toxins stored in our body. On the other hand, our brain has its own maintenance system called the glymphatic system, which uses the cells’ mitochondria to remove cellular waste from the brain. Cleansing your body and your brain from these contaminations can improve your brain function and overall well-being.</p>
-              <h4 class='text-uppercase text-center'>Superfoods</h4>
-              <hr class="w-50 ml-0 mb-3 mx-auto" />
-              <a class='' name='coconut-oil'><h5 class='text-uppercase'>Coconut oil</h5></a>
-              <p>Unrefined organic coconut oil considered the gold standard. It is extracted from fresh coconut using a wet-milled fermentation process that protects the beneficial properties of the coconut. The 62% is made up of a combination of the 4 types of medium-chain fatty acids(C6, C8, C10, C12) mainly Luric acid (C12).</p>
-              <a class='' name='mct-oil'><h5 class='text-uppercase'>MCT oil</h5></a>
-              <p>MCT oil is extracted from Coconut oil which is concentrated and has only C8 and C10. MCTs are easily digested and absorbed. Therefore, they provide nourishment to the body and are a quick source of energy for body and brain [14].</p>
-              <a class='' name='activated-charcoal'><h5 class='text-uppercase'>Activated charcoal</h5></a>
-              <p></p>
-              <a class='' name='acai-berries'><h5 class='text-uppercase'>Acai berries</h5></a>
-              <p></p>
-              <a class='' name='goji-berries'><h5 class='text-uppercase'>Goji berries</h5></a>
-              <p></p>
-              <a class='' name='flax-seed'><h5 class='text-uppercase'>Flax seeds</h5></a>
-              <p></p>
-              <a class='' name='sesame-seed'><h5 class='text-uppercase'>Sesame seeds</h5></a>
-              <p></p>
-              <a class='' name='chia-seed'><h5 class='text-uppercase'>Chia seeds</h5></a>
-              <p></p>
-              <a class='' name='raw-cacao'><h5 class='text-uppercase'>Raw Cacao</h5></a>
-              <p>Sun-dried raw cacao powder is made by grinding up raw cacao beans, then extracting the pure oil in a cold pressing process. The result is a silky smooth powder with deep sensual notes of flavour.</p>
-              <a class='' name='beetroot'><h5 class='text-uppercase'>beetroot</h5></a>
-              <p></p>
-              <a class='' name='green-tea'><h5 class='text-uppercase'>Green tea</h5></a>
-              <p></p>
-              <a class='' name='coffee'><h5 class='text-uppercase'>Coffee</h5></a>
-              <p>Caffeine in coffee is a central nervous system stimulant that is consumed all over the world to boost alertness and fight fatigue. It works by blocking the effects of a neurotransmitter called adenosine, which builds up over the course of the day and makes you feel tired [16]. </p>
-              <a class='' name='matcha'><h5 class='text-uppercase'>matcha</h5></a>
-              <p></p>
-              <a class='' name='stevia'><h5 class='text-uppercase'>stevia</h5></a>
-              <p></p>
-              <a class='' name='hemp'><h5 class='text-uppercase'>hemp</h5></a>
-              <p></p>
-              <h4 class='text-uppercase text-center'>Green Algae</h4>
-              <hr class="w-50 ml-0 mb-3 mx-auto" />
-              <a class='' name='chlorella'><h5 class='text-uppercase'>Chlorella</h5></a>
-              <p></p>
-              <a class='' name='spirulina'><h5 class='text-uppercase'>Spirulina</h5></a>
-              <p></p>
-              <h4 class='text-uppercase text-center'>Medicinal mushrooms</h4>
-              <hr class="w-50 ml-0 mb-3 mx-auto" />
-              <p>We use dual extracted mushrooms 10:1. This process is required in order to release the bioactive ingredients of mushroom and prepare it for human consumption.</p>
-              <a class='' name='lions-mane-mushroom'><h5 class='text-uppercase'>Lion's mane mushroom</h5></a>
-              <p>Lion’s Mane increases the amount of NGF(Nerve Growth Factor) [13] in the brain, which enhances cognition by reducing inflammation, encouraging neural growth, and improving overall brain health [12].</p>
-              <a class='' name='chaga-mushroom'><h5 class='text-uppercase'>Chaga mushroom</h5></a>
-              <p></p>
-              <h4 class='text-uppercase text-center'>Functional herbs</h4>
-              <hr class="w-50 ml-0 mb-3 mx-auto" />
-              <a class='' name='guto-kola'><h5 class='text-uppercase'>Guto kola</h5></a>
-              <p></p>
-              <a class='' name='milk-thistle'><h5 class='text-uppercase'>Milk thistle</h5></a>
-              <p></p>
-              <a class='' name='turmeric'><h5 class='text-uppercase'>Turmeric</h5></a> <p></p> <a class='' name='cinnamon'><h5 class='text-uppercase'>Cinnamon</h5></a>
-              <p></p>
-              <a class='' name='carob'><h5 class='text-uppercase'>Carob</h5></a>
-              <p></p>
-              <a class='' name='ginseng'><h5 class='text-uppercase'>Ginseng</h5></a>
-              <p></p>
-              <a class='' name='curcumin'><h5 class='text-uppercase'>Curcumin</h5></a>
-              <p></p>
-              <a class='' name='guto-kola'><h5 class='text-uppercase'>Gotu kola</h5></a>
-              <p></p>
-              <a class='' name='ginko-leaf'><h5 class='text-uppercase'>Ginko leaf</h5></a>
-              <p></p>
-              <a class='' name='ashwagandha'><h5 class='text-uppercase'>Ashwagandha</h5></a>
-              <p></p>
-              <h4 class='text-uppercase text-center'>Probiotics</h4>
-              <hr class="w-50 ml-0 mb-3 mx-auto" />
-              <a class='' name='lactobacillus'><h5 class='text-uppercase'>Lactobacillus</h5></a>
-              <p></p>
             </div>
             <div class="col-md-6">
               <Benefits />
-              <h5>References</h5>
-              <ul class='text-sm list-unstyled mx-2'>
-               <li>[1] <a class='text-black' href='https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2805706/' target='_blank'>The effects of nutrients on brain function</a> </li>
-               <li>[2] <a class='text-black' href='https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5641835/' target='_blank'>Gut microbiota’s effect on mental health</a> </li>
-               <li>[3] <a class='text-black' href='https://www.ncbi.nlm.nih.gov/pubmed/25550456' target='_blank'>Gut microbes promote colonic serotonin production</a> </li>
-               <li>[4] <a class='text-black' href='https://www.ncbi.nlm.nih.gov/pubmed/28483500' target='_blank'>Probiotic Bifidobacterium longum reduces depression</a> </li>
-               <li>[5] <a class='text-black' href='http://www.pnas.org/content/108/38/16050' target='_blank'>Ingestion of Lactobacillus strain regulates emotional behavior</a> </li>
-               <li>[6] <a class='text-black' href='https://www.alzforum.org/news/research-news/brain-drain-glymphatic-pathway-clears-av-requires-water-channel' target='_blank'>"Glymphatic" Pathway Clears Aβ, Requires Water Channel</a> </li>
-               <li>[7] <a class='text-black' href='http://www.cell.com/cell-metabolism/abstract/S1550-4131%2813%2900454-3' target='_blank'>Inflammation-Induced Alteration of Astrocyte Mitochondrial ...</a> </li>
-               <li>[8] <a class='text-black' href='https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4636982' target='_blank'>The Glymphatic System – A Beginner's Guide</a> </li>
-               <li>[9] <a class='text-black' href='https://www.ncbi.nlm.nih.gov/pubmed/24409051' target='_blank'>Sleep, immunity and inflammation in gastrointestinal disorders</a></li>
-               <li>[10] <a class='text-black' href='https://www.ncbi.nlm.nih.gov/pubmed/20329590' target='_blank'>Essential fatty acids and human brain</a></li>
-               <li>[11] <a class='text-black' href='https://www.ncbi.nlm.nih.gov/pubmed/20564545' target='_blank'>Milk thistle in liver diseases</a></li>
-               <li>[12] <a class='text-black' href='https://www.ncbi.nlm.nih.gov/pubmed/18844328' target='_blank'>Improving effects of the mushroom Yamabushitake on mild ...</a></li>
-               <li>[13] <a class='text-black' href='https://www.ncbi.nlm.nih.gov/gene/4803' target='_blank'>NGF nerve growth factor [Homo sapiens(human)]</a></li>
-               <li>[14] <a class='text-black' href='https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2604900' target='_blank'>Hypometabolism as a therapeutic target in Alzheimer's disease</a></li>
-               <li>[15] <a class='text-black' href='https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4960264' target='_blank'>Neuroplasticity and Clinical Practice: Building Brain...</a></li>
-               <li>[16] <a class='text-black' href='https://www.ncbi.nlm.nih.gov/pubmed/11283304' target='_blank'>The role and regulation of adenosine in the central nervous system
-      
-      </a></li>
-              </ul>
             </div>
           </div>
         </div>
